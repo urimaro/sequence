a27105b768024f8a0b4a9d261e416db4c49ca863
Goh Matsumoto <urimaro@gmail.com>
2018-09-22 16:05:49 +0900

    P196

--- lib/sequence/server.ex
+++ lib/sequence/server.ex
@@ -4,5 +4,9 @@ defmodule Sequence.Server do
   def handle_call(:next_number, _from, current_number) do
     {:reply, current_number, current_number + 1}
   end
+
+  def handle_call({:set_number, new_number}, _from, _current_number) do
+    {:reply, new_number, new_number}
+  end
 end
 
